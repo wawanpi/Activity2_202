d771e2dda87c474d7a4ece142be0255361e59ba6
wawanpi <muhaguskurniawan@gmail.com>
2023-03-20 15:49:30 +0700

    menampilkan data berupa pecaha

--- Latihan4_202/Latihan4_202.cpp
+++ Latihan4_202/Latihan4_202.cpp
@@ -4,7 +4,7 @@ using namespace std;
 float LuasPersegi(float p, float l) {
     return p * l;
 }
-float LuasLingkaran(float r) {
+float LuasLingkaran(float r){
     return 3.14 * r * r;
 }
 float LuasSegitiga(float a, float t) {
