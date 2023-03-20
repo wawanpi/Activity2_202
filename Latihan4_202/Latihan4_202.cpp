dc80bdd8e15c1ea1fdcd7d695a5022147e7668e5
wawanpi <muhaguskurniawan@gmail.com>
2023-03-20 15:55:36 +0700

    float panjang

--- Latihan4_202/Latihan4_202.cpp
+++ Latihan4_202/Latihan4_202.cpp
@@ -10,4 +10,19 @@ float LuasLingkaran(float r) {
 float LuasSegitiga(float a, float t) {
     return 0, 5 * a * t;
 }
+int main()
+{
+    float panjang, lebar, jejari, alas, tinggi;
+    int pilihan;
+
+    do {
+        cout << "\n\n=============== " << endl;
+        cout << "======= MENU ===== " << endl;
+        cout << "================= " << endl;
+        cout << "1. Luas Persegi " << endl;
+        cout << "2. Luas Lingkaran " << endl;
+        cout << "3. Luas Segitiga " << endl;
+        cout << "4. Exit" << endl;
+        cin >> pilihan;
+
 
