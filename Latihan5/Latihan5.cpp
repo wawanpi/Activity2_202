f0ac6d62a59db82e6e10c57a1ee712f7a8e000c5
wawanpi <muhaguskurniawan@gmail.com>
2023-03-20 15:38:35 +0700

    float

---
+++ Latihan5/Latihan5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a[5];
+    cout << "Mengisi Array" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Data ke-" << (i) << ":";
+
+        cin >> a[i];
+
+    }
+    cout << "Membaca Array" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Data ke-" << (i) << ": " << a[i] << endl;
+    }
+}
